e5ab770393005edbf3d6de888ef423b6639c6e07
KAAA HOSTING <kaaahosting@gmail.com>
2026-05-12 14:56:33 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Simpan semua rahasia di sini
-export const config = {
-    MONGODB_URI: "mongodb+srv://eka710231_db_user:kaaacloud@cluster0.6ssgy30.mongodb.net/?appName=Cluster0",
-    RESEND_API_KEY: "re_Y3DfKKCM_7rJCyA3V1JHoxUNxQ2sc7Fb1", // GANTI DENGAN API KEY RESEND KAMU
-    TELEGRAM_BOT_TOKEN: "8622340136:AAE9DCnzLeF48qO86IzPEDDTkGlP29_YNeY", // GANTI DENGAN TOKEN BOT KAMU
-    TELEGRAM_CHAT_ID: "1955836653", // GANTI DENGAN ID TELEGRAM KAMU
-    SENDER_EMAIL: "otp@kaaaoffc.web.id",
-    DOWNLOAD_URL: "https://www.mediafire.com/file/k001s9hokumihr1/SC+SATURN07+NO+ENC+V2.3.zip/file"
-};
