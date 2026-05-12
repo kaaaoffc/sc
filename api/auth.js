3f4a0489ac4fb9eeb31f3b636171b4da765055f4
KAAA HOSTING <kaaahosting@gmail.com>
2026-05-12 14:45:46 +0700

    Change email sender address in OTP email

--- api/auth.js
+++ api/auth.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
         // Kirim Email via Resend
         await resend.emails.send({
-            from: 'admin@kaaaoffc.web.id',
+            from: 'code@kaaaoffc.web.id',
             to: email,
             subject: 'Kode OTP SC SATURN',
             html: `Your OTP code is: <strong>${generatedOtp}</strong>`
